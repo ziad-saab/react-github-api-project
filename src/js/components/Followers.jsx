a4fbad8c5140a94c402800ae44d2bcc0067e2556
Katarina <klingat@me.com>
2016-08-11 19:36:16 +0000

    added infinite scrolling to follower/following

--- src/js/components/Followers.jsx
+++ src/js/components/Followers.jsx
@@ -1,43 +1,48 @@
 var React = require('react');
 var $ = require("jquery");
 var GithubUser = require("./GithubUser.jsx");
+var Infinite = require('react-infinite');
 
 
 
 var Followers = React.createClass({
     getInitialState: function(){
         return {
+            page: 1,
+            loading: false,
             followers: []
         };
     },
-    componentDidMount: function() {
+    fetchData: function() {
         var that = this;
-        $.getJSON(`https://api.github.com/users/${this.props.params.username}/followers?access_token=6a8cf53c2aa5c4ae828dd476cce0a58f7c069def`)
-            .done(
-                
+        this.setState({
+            loading: true
+        })
+        $.getJSON(`https://api.github.com/users/${this.props.params.username}/followers?access_token=6a8cf53c2aa5c4ae828dd476cce0a58f7c069def&page=${this.state.page}&per_page=50`)
+            .then(
                 function(followers) {
-                    console.log(followers, "NOT HAPPENING")
+                    console.log(followers, "checking checking checking")
                     that.setState({
-                        followers: followers
+                        followers: that.state.followers.concat(followers),
+                        loading: false,
+                        page: (that.state.page + 1)
                     });
                 }
-            ).fail(function(err){
+            ).catch(function(err){
                 console.log(err);
             })
     },
     render: function() {
-        if (!this.state.followers) {
-            return <div>LOADING FOLLOWERS...</div>
-        }
-        console.log(this.state.followers)
         return (
         <div className="followers-page">
             <h2>People that follow {this.props.params.username}:</h2>
-            <ul>
-                {this.state.followers.map(function(user){
+            <Infinite isInfiniteLoading={this.state.loading} onInfiniteLoad={this.fetchData} useWindowAsScrollContainer elementHeight={90} infiniteLoadBeginEdgeOffset={100}>
+                
+                {this.state.followers.map(function(user) {
                     return <GithubUser key={user.id} user={user}/>
                 })}
-            </ul>
+                
+            </Infinite>
         </div>
         );
     }
--- src/js/components/Following.jsx
+++ src/js/components/Following.jsx
@@ -1,43 +1,48 @@
 var React = require('react');
 var $ = require("jquery");
 var GithubUser = require("./GithubUser.jsx");
+var Infinite = require('react-infinite');
 
 
 
 var Following = React.createClass({
     getInitialState: function(){
         return {
+            page: 1,
+            loading: false,
             following: []
         };
     },
-    componentDidMount: function() {
+    fetchData: function() {
         var that = this;
-        $.getJSON(`https://api.github.com/users/${this.props.params.username}/following?access_token=6a8cf53c2aa5c4ae828dd476cce0a58f7c069def`)
-            .done(
-                
+        this.setState({
+            loading: true
+        })
+        $.getJSON(`https://api.github.com/users/${this.props.params.username}/following?access_token=6a8cf53c2aa5c4ae828dd476cce0a58f7c069def&page=${this.state.page}&per_page=50`)
+            .then(
                 function(following) {
-                    console.log(following, "NOT HAPPENING")
+                    console.log(following, "checking checking checking")
                     that.setState({
-                        following: following
+                        following: that.state.following.concat(following),
+                        loading: false,
+                        page: (that.state.page + 1)
                     });
                 }
-            ).fail(function(err){
+            ).catch(function(err){
                 console.log(err);
             })
     },
-    render: function() {
-        if (!this.state.following) {
-            return <div>LOADING FOLLOWERS...</div>
-        }
-        console.log(this.state.following)
+   render: function() {
         return (
         <div className="followers-page">
             <h2>People that {this.props.params.username} is following:</h2>
-            <ul>
-                {this.state.following.map(function(user){
+            <Infinite isInfiniteLoading={this.state.loading} onInfiniteLoad={this.fetchData} useWindowAsScrollContainer elementHeight={90} infiniteLoadBeginEdgeOffset={100}>
+                
+                {this.state.following.map(function(user) {
                     return <GithubUser key={user.id} user={user}/>
                 })}
-            </ul>
+                
+            </Infinite>
         </div>
         );
     }
