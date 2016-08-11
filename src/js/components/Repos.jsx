48001f6a9c1c19676153b8ce1ec4ab6243e51cbc
Katarina <klingat@me.com>
2016-08-11 19:40:37 +0000

    added inifnite scroll to repos

--- src/js/components/Repos.jsx
+++ src/js/components/Repos.jsx
@@ -1,41 +1,46 @@
 var React = require('react');
 var $ = require("jquery");
 var GithubRepo = require("./GithubRepo.jsx");
+var Infinite = require('react-infinite');
 
 var Repos = React.createClass({
     getInitialState: function(){
         return {
+            page: 1,
+            loading: false,
             repos: []
         };
     },
-    componentDidMount: function() {
+    fetchData: function() {
+        this.setState({
+            loading: true
+        })
         var that = this;
-        $.getJSON(`https://api.github.com/users/${this.props.params.username}/repos?access_token=6a8cf53c2aa5c4ae828dd476cce0a58f7c069def`)
-            .done(
-                
+        $.getJSON(`https://api.github.com/users/${this.props.params.username}/repos?access_token=6a8cf53c2aa5c4ae828dd476cce0a58f7c069def&page=${this.state.page}&per_page=50`)
+            .then(
                 function(repos) {
-                    console.log(repos[0].name, "heererrfr")
                     that.setState({
-                        repos: repos
+                        repos: that.state.repos.concat(repos),
+                        loading: false,
+                        page:(that.state.page + 1)
                     });
                 }
-            ).fail(function(err){
+            ).catch(function(err){
                 console.log(err);
             })
     },
     render: function() {
-        if (!this.state.repos) {
-            return <div>LOADING REPOS...</div>
-        }
-        console.log(this.state.repos)
         return (
         <div className="followers-page">
             <h2>{this.props.params.username}'s repos:</h2>
-            <ul>
-                {this.state.repos.map(function(repo){
-                    return (<GithubRepo key={repo.id} repo={repo}/>)
+            
+            <Infinite isInfiniteLoading={this.state.loading} onInfiniteLoad={this.fetchData} useWindowAsScrollContainer elementHeight={20} infiniteLoadBeginEdgeOffset={100}>
+                
+                {this.state.repos.map(function(repo) {
+                    return <GithubRepo key={repo.id} repo={repo}/>
                 })}
-            </ul>
+                
+            </Infinite>
         </div>
         );
     }
