90ba2d1f08d5645d41cd8f38c7196b464277ae5a
Ziad Saab <ziad.saab@gmail.com>
2016-09-07 22:54:58 +0000

    infinite

--- src/js/components/Followers.jsx
+++ src/js/components/Followers.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 // We're using jQuery to make AJAX calls because that's what we're used to for the moment
 // Later on you might want to use a lighter library that only does AJAX, like isomorphic-fetch or superagent
 var $ = require('jquery');
+var Infinite = require('react-infinite');
 
 var GithubUser = require('./GithubUser');
 
@@ -13,7 +14,11 @@ var Followers = React.createClass({
         })
     },
     getInitialState: function() {
-        return {};
+        return {
+            page: 1,
+            loading: false,
+            followers: []
+        };
     },
     /*
     This method will be called by React after the first render. It's a perfect place to load
@@ -27,36 +32,45 @@ var Followers = React.createClass({
     the data -- in the callback -- we call `setState` to put the user data in our state. This will trigger a re-render.
     When `render` gets called again, `this.state.user` exists and we get the user info display instead of "LOADING..."
     */
-    componentDidMount: function() {
-        var that = this; // What's this?? Make sure you remember or understand what this line does
+    fetchData: function() {
+        if (this.state.done) {return;}
+        this.setState({loading: true});
         
-        $.getJSON(`https://api.github.com/users/${this.props.params.username}/followers`)
+        var that = this; // What's this?? Make sure you remember or understand what this line does
+        var page = this.state.page;
+        $.getJSON(`https://api.github.com/users/${this.props.params.username}/followers?per_page=25&page=${page}&access_token=ef35a1650fec29e13d9782afc34c6138b0213ec8`)
             .then(
                 function(followers) {
-                    // Why that.setState instead of this.setState??
-                    that.setState({
-                        followers: followers
-                    });
+                    if (followers.length === 0) {
+                        that.setState({
+                            loading: false,
+                            done: true
+                        });
+                    }
+                    else {
+                        // Why that.setState instead of this.setState??
+                        that.setState({
+                            followers: that.state.followers.concat(followers),
+                            page: page + 1,
+                            loading: false
+                        });    
+                    }
+                    
                 }
             );
     },
     render: function() {
-        // If the state doesn't have a followers key, it means the AJAX didn't complete yet. Simply render a LOADING indicator.
-        if (!this.state.followers) {
-            return (<div className="followers-page">LOADING...</div>);
-        }
+        var items = this.state.followers.map(function(user) {
+            return <div className="followers-list__item" key={user.id}><GithubUser user={user}/></div>;
+        });
         
         // Look in app.css for the styles that make this look like it does
         return (
             <div className="followers-page">
                 <h3>Followers of {this.props.params.username}</h3>
-                <ul className="followers-list">
-                {
-                    this.state.followers.map(function(user) {
-                        return <li key={user.id}><GithubUser user={user}/></li>;
-                    })
-                }
-                </ul>
+                <Infinite loadingSpinnerDelegate={<div className="loading"/>} isInfiniteLoading={this.state.loading} onInfiniteLoad={this.fetchData} infiniteLoadBeginEdgeOffset={this.state.done ? undefined : 100} elementHeight={41} useWindowAsScrollContainer>
+                    {items}
+                </Infinite>
             </div>
         );
     }
