4a5d5329aa2190fd5ea092d4c69051d35c539d8b
mscale92 <scalemary@gmail.com>
2016-11-10 10:33:52 -0500

    ajax worked, fixed this to that

--- src/js/components/Followers.jsx
+++ src/js/components/Followers.jsx
@@ -1,16 +1,22 @@
 var React = require('react');
 var Link = require('react-router').Link;
+var $ = require('jquery');
+
 
 var Followers = React.createClass({
 	componentDidMount: function(){
+		var that = this;
+			// that is the this referring to our component
 		 $.getJSON(`https://api.github.com/users/${this.props.params.username}/followers?access_token=8dbd67ccdec639d5803b020db060a7e3d5be27cc`)
 		 .then(function(result){
-		 	setState({
-		 		followers: followers
+		 	console.log(that.props.params.username);
+		 	that.setState({
+		 		followers: result
 		 	});
 		 });
 	},
 	render: function(){
+		
 		return (
 			<div className="followers-page">
 			  <h3>Followers of USERNAME</h3>
---
+++ src/js/components/GithubUser.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+		
+var GithubUser = React.createClass({
+	render: function(){
+		return (null);
+	}
+});
+
+module.exports = GithubUser;
