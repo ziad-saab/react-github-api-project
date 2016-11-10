d776f5e906cbf163baa95c04eb7e617f0e17d27e
mscale92 <scalemary@gmail.com>
2016-11-10 10:16:59 -0500

    did the ajax call for followers

--- src/js/components/Followers.jsx
+++ src/js/components/Followers.jsx
@@ -3,7 +3,12 @@ var Link = require('react-router').Link;
 
 var Followers = React.createClass({
 	componentDidMount: function(){
-
+		 $.getJSON(`https://api.github.com/users/${this.props.params.username}/followers?access_token=8dbd67ccdec639d5803b020db060a7e3d5be27cc`)
+		 .then(function(result){
+		 	setState({
+		 		followers: followers
+		 	});
+		 });
 	},
 	render: function(){
 		return (
