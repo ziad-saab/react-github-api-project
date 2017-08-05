989e4afadca24a5c11f7c1f6729bdce0040236e4
Marissa Beatty <beattymarissa@gmail.com>
2017-08-05 13:58:58 +0000

    scrolling works on Followers page!

--- src/components/Followers.jsx
+++ src/components/Followers.jsx
@@ -4,74 +4,76 @@ import GithubUser from './GithubUser';
 import Infinite from 'react-infinite';
 
 class Followers extends React.Component {
-    
-    constructor() {
-        super();
-        this.state = {
-            page: 1, 
-            loading: false, 
-            followers: []
-        };
-    }
-    
-    fetchData() {
-        this.setState = {
-            loading: true
+
+ constructor() {
+    super();
+    this.state = {
+      page: 1,
+      loading: false,
+      followers: []
+    };
+}
+
+ fetchData = () => {
+
+   this.setState({
+      loading: true
+    });
+    //console.log(this)
+    fetch(`https://api.github.com/users/${this.props.params.username}/followers`)
+      .then(response => response.json())
+      .then(
+        followersResponse => {
+          console.log('got response');
+
+         let mergedArray = this.state.followers.concat(followersResponse);
+          this.setState({
+            followers: mergedArray,
+            page: this.state.page + 1,
+            loading: false
+          });
         }
-        // console.log(this, "this before AJAX call")
-        //https://api.github.com/users/USER/followers?access_token=XXX&page=1&per_page=50
-        fetch(`https://api.github.com/users/${this.props.params.username}/followers?&page=${this.state.page}&per_page=50`)
-        
-        .then(response => response.json())
-        // this.setState({list: this.state.list.concat([newObject])});
-        // .then(console.log(this, "this after AJAX call"))
-        .then(
-            followers => {
-                console.log("before set state", followers, this.state.followers.concat(followers))
-                this.setState = {
-                    followers: this.state.followers.concat(followers), 
-                    loading: false, 
-                    page: this.state.page + 1
-                };
-            }
-        )
-        // .then(
-        //     console.log(this)
-        // )
+      );
+    // .then(
+    //     console.log(this)
+    // )
+  }
+  //
+  // componentDidMount() {
+  //   this.fetchData();
+  // }
+
+ componentDidUpdate(prevProps, prevState) {
+    if (prevProps.params.username !== this.props.params.username) {
+      this.fetchData();
     }
-    
-    componentDidUpdate(prevProps, prevState) {
-        console.log('update');
-        if (prevProps.params.username !== this.props.params.username) { 
-          this.fetchData();
-      }
-    }
-    
-    render() {
-        // if (!this.state.followers) {
-        //     return <div>LOADING FOLLOWERS...</div>;
-        // }
-        console.log('render', this.state.followers)
-        return (
-            <Infinite className="followers-page" 
-            isInfiniteLoading={this.state.loading} 
-            onInfiniteLoad={this.fetchData.bind(this)} 
-            useWindowAsScrollContainer 
-            elementHeight={100} 
-            infiniteLoadBeginEdgeOffset={100} >
-//             isInfiniteLoading: take it from your loading state
-// onInfiniteLoad: point to your fetchData method
-// useWindowAsScrollContainer: this prop doesn't have a value! It will be set to true automatically
-// elementHeight: to scroll efficiently, React Infinite needs to know the height of an element. Use your browser's inspector to find the approximate height of your GithubUser elements. It's not perfect, but it'll do for now.
-// infiniteLoadBeginEdgeOffset: this sets the amount of pixels from the edge of your container at which more data will be loaded. Set it to 
-            
-                  {this.state.followers.map(follower => {
-                  console.log('wtf', follower)
-                  return <GithubUser user={follower} key={follower.id} />
-                      
-                  } )}
+  }
+
+ render() {
+    console.log('in render', this.state.followers);
+    //
+    // if (!this.state.followers) {
+    //   return <div>LOADING followers...</div>;
+    // }
+    return (
+      <div className="followers-page">
+        <h3>Followed by {this.props.params.username}</h3>
+        <div className="Infinite-scroll-followers">
+            <Infinite className="followers-page"
+                      isInfiniteLoading={this.state.loading}
+                      onInfiniteLoad={this.fetchData.bind(this)}
+                      useWindowAsScrollContainer
+                      elementHeight={100}
+                      infiniteLoadBeginEdgeOffset={100} >
+                {this.state.followers.map(follower => {
+                      return <GithubUser user={follower.login} key={follower.id} avatar_url={follower.avatar_url} /> 
+                    
+                }
+                )} 
             </Infinite>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 export default Followers;
